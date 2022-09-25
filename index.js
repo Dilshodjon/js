d6c7f3b6a3deffa5e20347d2eab36ec7cb753598
Dilshodbek <112108374+Dilshodjon@users.noreply.github.com>
2022-09-25 23:02:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// let fistName = "Obama";
+// let number = 15;
+// let isMerried = false;
+// let undefined;
+
+// console.log(fistName);
+// console.log(number);
+// console.log(isMerried);
+// let name = "ibrahgim";
+// let ege = 15;
+// let person = {
+//   name: "ibragim",
+//   age: 15,
+// }
+// let person = {
+//    name: "Zumrad",
+//    age: 40,
+//    isMerried: "true",
+  
+// }
+// object
+// let person = {
+//   name:"zumrad",
+//   age:15,
+// }
+// person.name = "dilshod";
+// person.age = 45
+// console.log(person);
+// array
+let selectedColor = ["oq","qora"];
+selectedColor[2] = "yashil";
+
+console.log(selectedColor.length);
