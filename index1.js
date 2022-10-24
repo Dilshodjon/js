3a62d39f3a1dfaa9700ac028a51942e5cff04ce5
Dilshodbek <112108374+Dilshodjon@users.noreply.github.com>
2022-10-24 22:41:07 +0500

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,16 @@
+const TezlikMax = 70;
+const km_cheklov = 5;
+const max_shtraf = 12;
+
+tezlikShtraf(150);
+
+function tezlikShtraf(tezlik) {
+  if (tezlik < TezlikMax) console.log("ok");
+  else {
+    let shtraf = Math.floor((tezlik - TezlikMax) / km_cheklov);
+    if (shtraf > max_shtraf) console.log("Tamom");
+    else {
+      console.log(shtraf);
+    }
+  }
+}
